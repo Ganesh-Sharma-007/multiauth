d680ceda279a049486484607ede45fc856e5e9ee
Ganesh-Sharma-007 <ganeshoffice086407@gmail.com>
2025-05-09 14:56:51 +0100

    multiauth project start

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const generateToken = require('../utils/generateToken');
+
+exports.register = async(req, res) => {
+    const { name, email, password, role } = req.body;
+    try{
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({ message: 'Email already exists' });
+        };
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashedPassword, role });
+        const token = generateToken(user);
+        res.cookie('token', token,{httpOnly: true}).status(201).json({user});
+    }catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
+
+exports.login = async(req, res) => {
+     const {email, password} = req.body;
+     try{
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({message: 'Invalid email or password'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({message: 'Invalid email or password'});
+        }
+
+        const token = generateToken(user);
+        res.cookie('token', token,{httpOnly: true}).status(200).json({user});
+     } catch (err) {
+        res.status(500).json({message: err.message})
+     }
+}
