7b61a193fd912c7c7d9aa23e305d3ce4f6d36005
Ganesh-Sharma-007 <ganeshoffice086407@gmail.com>
2025-05-09 15:34:42 +0100

    multi auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,39 +2,43 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const generateToken = require('../utils/generateToken');
 
-exports.register = async(req, res) => {
+exports.register = async (req, res) => {
     const { name, email, password, role } = req.body;
-    try{
+    try {
         const existingUser = await User.findOne({ email });
-        if(existingUser) {
+        if (existingUser) {
             return res.status(400).json({ message: 'Email already exists' });
         };
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({ name, email, password: hashedPassword, role });
         const token = generateToken(user);
-        res.cookie('token', token,{httpOnly: true}).status(201).json({user});
-    }catch (err) {
-        res.status(500).json({message: err.message})
+        res.cookie('token', token, { httpOnly: true }).status(201).json({ user });
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 }
 
 
-exports.login = async(req, res) => {
-     const {email, password} = req.body;
-     try{
-        const user = await User.findOne({email});
-        if(!user) {
-            return res.status(400).json({message: 'Invalid email or password'});
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) {
-            return res.status(400).json({message: 'Invalid email or password'});
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         const token = generateToken(user);
-        res.cookie('token', token,{httpOnly: true}).status(200).json({user});
-     } catch (err) {
-        res.status(500).json({message: err.message})
-     }
+        res.cookie('token', token, { httpOnly: true }).status(200).json({ user });
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+exports.getMe = async (req, res) => {
+    res.json(req.user);
 }
